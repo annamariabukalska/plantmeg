f2de0bb38baa819348809324eaca3caee03ffc2b
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-05-23 19:21:38 +0200

    link til handlekurven via React Router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar'
+import CartPage from './pages/CartPage';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <NavBar/>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/CartPage" component={CartPage} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
