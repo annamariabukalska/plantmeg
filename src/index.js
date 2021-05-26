bac85b3d99af0804c580e1434ea24d4281689458
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-05-26 19:21:25 +0200

    Flytter Pages til egne mapper

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import CartPage from "./pages/CartPage";
+import CartPage from "./pages/CartPage/CartPage";
 import ErrorPage from "./pages/ErrorPage";
 import Footer from "./components/Footer/Footer";
-import ProductPage from "./pages/ProductPage";
-import CategoryPage from "./pages/CategoryPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import CategoryPage from "./pages/CategoryPage/CategoryPage";
 
 ReactDOM.render(
   <BrowserRouter>
