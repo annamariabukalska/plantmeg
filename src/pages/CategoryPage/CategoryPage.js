4cf2139ad0a0eab02989499a819167e13bedb813
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-05-27 16:11:00 +0200

    wip CategoryPage

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -1,9 +1,18 @@
+import CategoryButton from "../../components/CategoryButton/CategoryButton";
+import Header from "../../components/Header/Header";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import "./CategoryPage.css";
 
 function CategoryPage() {
   return (
     <div>
-      <p>Kategorie kommer her straks</p>
+      <h1>Inneplanter</h1>
+      <h3>Inneplanter skaper et levende hjem og gir bedre luft. Hos oss finner du alltid herlig blomstrende og friske grønne planter i forskjellige former og størrelser som passer i alle hjem. Med levende planter i hjemmet får du et renere og friskere innemiljø.</h3>
+      <CategoryButton />
+      <img src={"images/plante-category.png"} alt="plants" />
+      <Header 
+      title="Blomstrende inneplanter"/>
+      <ProductCard />
     </div>
   );
 }
