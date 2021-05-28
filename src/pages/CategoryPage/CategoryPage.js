3311e0633d949dd797381b2fa9b4a73efc5028d3
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-05-28 17:27:40 +0200

    wip CategoryPage

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -8,10 +8,25 @@ function CategoryPage() {
     <div>
       <h1>Inneplanter</h1>
       <h3>Inneplanter skaper et levende hjem og gir bedre luft. Hos oss finner du alltid herlig blomstrende og friske grønne planter i forskjellige former og størrelser som passer i alle hjem. Med levende planter i hjemmet får du et renere og friskere innemiljø.</h3>
-      <CategoryButton />
-      <img src={"images/plante-category.png"} alt="plants" />
+      <div className="categorybutton-grid">
+      <CategoryButton 
+      title="Grønne planter"/>
+       <CategoryButton 
+      title="Blomstrende inneplanter"/>
+       <CategoryButton 
+      title="Buketter og snittblomster"/>
+      <CategoryButton 
+      title="Stiklinger fra potteplanter"/>
+      </div>
+      <img className="img-category" src={"images/plante-category.png"} alt="plants" />
+      <Header 
+      title="Grønne planter"/>
       <Header 
       title="Blomstrende inneplanter"/>
+      <Header 
+      title="Buketter og snittblomster"/>
+      <Header 
+      title="Stiklinger fra potteplanter"/>
       <ProductCard />
     </div>
   );
