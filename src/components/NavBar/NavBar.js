b2974c06de81f8d60e5c1e692e2fd2d557227f35
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-05-28 23:51:48 +0200

    added ul i  NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,20 +7,30 @@ function NavBar() {
       <Link to="/">
         <img src="/images/plantmeg-logo.png" alt="søke" className="logo" />
       </Link>
+      <ul className="menu">
+        <Link to="/">
+          <li>Våre planter</li>
+        </Link>
+        <Link to="/">
+          <li>Nyheter</li>
+        </Link>
+        <Link to="/">
+          <li>Tips og råd</li>
+        </Link>
+        
+        <Link to="/handlekurv">
+          <img
+            src="/images/shopping-cart.png"
+            alt="shoping-cart"
+            className="icons"
+          />
+       </Link>
+       </ul>
       <form>
         <label>
           <input type="text" placeholder="Søk" className="search-box"></input>
         </label>
       </form>
-      <div>
-      <Link to="/handlekurv">
-        <img
-          src="/images/shopping-cart.png"
-          alt="shoping-cart"
-          className="icons"
-        />
-      </Link>
-      </div>
     </nav>
   );
 }
